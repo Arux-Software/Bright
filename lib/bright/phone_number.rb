415b0f9b00b5d245cd17483e51b6d8e2b3029ec9
Steve Novotny <SNovotny@gmail.com>
2018-11-21 16:22:52 -0600

    add extension to phone number
    and try and break on an “x”/“X” when the input comes in.

--- lib/bright/phone_number.rb
+++ lib/bright/phone_number.rb
@@ -1,11 +1,19 @@
 module Bright
   class PhoneNumber < Model
-    @attribute_names = [:phone_number, :type]
+    @attribute_names = [:phone_number, :extension, :type]
     attr_accessor *@attribute_names
     TYPES = ["Cell", "Home", "Work", "Other"]
 
     def phone_number=(number)
-      @phone_number = number.gsub(/[^0-9]/, "")
+      number_a = number.split(/x|X/)
+      if number_a.size == 2
+        @extension = number_a.last.gsub(/[^0-9]/, "").strip
+      end
+      @phone_number = number_a.first.gsub(/[^0-9]/, "").strip
+    end
+
+    def extension=(number)
+      @extension = number.gsub(/[^0-9]/, "").strip
     end
 
   end
